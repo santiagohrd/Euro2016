9ae6ef0d88b4c52b3752bd61b851692b02454ae2
santiagohrd <santiagohrd1@gmail.com>
2023-09-03 06:22:37 -0500

    Se relacionan nuevas querys sobre clasificación de jugadores por edad y goles anotados en el torneo

--- EuroCopa Query.sql
+++ EuroCopa Query.sql
@@ -1,3 +1,4 @@
+-- Categorización de jugadores por edad
 SELECT nombre_jugador, edad, 
 CASE	
 	WHEN edad <23 THEN 'Joven'
@@ -31,7 +32,6 @@ WHERE id_pais = (
                 FROM paises
                 WHERE nombre_pais = 'Portugal')
 ;
-
 -- Máximo goleador del torneo
 SELECT *
 FROM jugadores
@@ -49,11 +49,80 @@ WHERE id_jugador NOT IN (
 						SELECT DISTINCT id_jugador
 						FROM goles);
                   
--- Jugadores que realizaron goles
+-- Jugadores que realizaron goles. Uso Subqueri correlacionado en este caso, sin embargo, también se podía usar un join
 SELECT *, 
 	(
     SELECT nombre_jugador
     FROM jugadores
     WHERE goles.id_jugador = jugadores.id_jugador
     ) AS Jugador
+FROM goles;
+
+-- Goles anotados por país
+SELECT nombre_pais
+, SUM(goles_anotados) as goles_anotados
+FROM partidos
+LEFT JOIN paises
+	ON partidos.id_pais = paises.id_pais
+GROUP BY nombre_pais
+ORDER BY goles_anotados desc;
+
+-- Info del jugador que hizo gol en la final
+SELECT jugadores.nombre_jugador
+, jugadores.playera
+, paises.nombre_pais
 FROM goles
+LEFT JOIN jugadores 
+	ON goles. id_jugador = jugadores. id_jugador
+LEFT JOIN paises 
+	ON goles.id_pais = paises.id_pais
+WHERE ronda = 'F';
+
+-- País mas goleado
+SELECT nombre_pais
+, goles_en_contra
+FROM equipos
+LEFT JOIN paises
+	ON equipos.id_pais = paises.id_pais
+WHERE goles_en_contra = (SELECT MAX(goles_en_contra) FROM equipos);
+
+-- Goles por partidos y jugadores
+SELECT g.num_partido,
+       p.nombre_pais AS nombre_país,
+       j.nombre_jugador AS nombre_jugador,
+       COUNT(*) AS cantidad_goles
+FROM goles g
+INNER JOIN paises p ON g.id_pais = p.id_pais
+INNER JOIN jugadores j ON g.id_jugador = j.id_jugador
+GROUP BY g.num_partido, p.nombre_pais, j.nombre_jugador
+ORDER BY g.num_partido;
+
+-- Conteo de goles por jugador
+SELECT nombre_jugador,
+	(SELECT nombre_pais FROM paises WHERE jugadores. id_pais = paises.id_pais) AS nombre_país,
+	club,
+	(SELECT COUNT(*) FROM goles WHERE goles. id_jugador = jugadores. id_jugador GROUP BY id_jugador) as goles_anotados
+FROM jugadores, goles
+WHERE jugadores. id_jugador = goles. id_jugador 
+GROUP BY jugadores. id_jugador
+ORDER BY goles_anotados desc;
+
+-- Jugadores mas jovenes y viejos por país
+SELECT nombre_pais,
+nombre_jugador,
+edad,
+CASE
+	WHEN edad = edad_max THEN 'Mas viejo'
+	WHEN edad = edad_min THEN 'Mas jóven'
+END as clasificación
+    
+FROM (SELECT
+	(SELECT nombre_pais FROM paises WHERE j1.id_pais = paises. id_pais) AS nombre_pais,
+	nombre_jugador ,
+	edad,
+	(SELECT MAX(edad) FROM jugadores as j2 WHERE j1.id_pais = j2.id_pais) as edad_max,
+	(SELECT MIN(edad) FROM jugadores as j2 WHERE j1.id_pais = j2.id_pais) as edad_min
+	FROM jugadores as j1
+	ORDER BY nombre_pais, edad desc) as clasificacion_jugadores
+WHERE edad = edad_max OR edad = edad_min;
+
